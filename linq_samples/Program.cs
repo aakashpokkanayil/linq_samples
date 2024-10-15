93cbe463e76380355e67765caf08c57baf0a0ee5
Aakash P <aakashpokkanayil@gmail.com>
2024-10-15 20:56:03 +0530

    Add project files.

---
+++ linq_samples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace linq_samples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
