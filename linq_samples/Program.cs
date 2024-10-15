fc4405b8ca95415160792a000495a3d4190e2333
Aakash P <aakashpokkanayil@gmail.com>
2024-10-15 22:08:43 +0530

    initial changes

--- linq_samples/Program.cs
+++ linq_samples/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var empList = new List<Employee>()
+            { 
+                new Employee(){
+                    Id = 1,
+                    Name = "Name1",
+                    City="City1",
+                    Job="Job1"
+
+                },
+                new Employee(){
+                    Id = 2,
+                    Name = "Name2",
+                    City="City2",
+                    Job="Job2"
+
+                },
+                new Employee(){
+                    Id = 3,
+                    Name = "Name3",
+                    City="City3",
+                    Job="Job1"
+
+                },
+                new Employee(){
+                    Id = 4,
+                    Name = "Name4",
+                    City="City1",
+                    Job="Job2"
+
+                },
+            };
+
+            var result=empList.Where(emp => emp.City == "City1");
+
+            foreach (Employee emp in result)
+            {
+                Console.WriteLine(emp.Name);
+            }
+
+
         }
     }
 }
